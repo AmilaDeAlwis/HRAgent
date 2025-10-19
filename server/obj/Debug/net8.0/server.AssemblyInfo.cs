116bf69cc4560d939cf23ced77adf3f91054fb8a
AmilaDeAlwis <amilaalwis4@gmail.com>
2025-10-19 23:44:55 +0530

    AppUser model created

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5b28678426d2805150fb1f33446273e609aeda6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94bc59ecfdf1e6f102c08b6db869650da9242865")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
