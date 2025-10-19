94bc59ecfdf1e6f102c08b6db869650da9242865
AmilaDeAlwis <amilaalwis4@gmail.com>
2025-10-19 23:41:24 +0530

    installed several nuget packages

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95ff057daa1e9a66e60ae49cf8fcc48168c242a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5b28678426d2805150fb1f33446273e609aeda6")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
